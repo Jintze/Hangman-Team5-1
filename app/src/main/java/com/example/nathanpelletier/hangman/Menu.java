62d6e2866496464092b9e9a96164e864af537288
EddyNK <EddyNK@github.com>
2017-11-21 00:38:34 -0700

    Had to change Menu.xml to menu.xml android studio is dumb.

--- app/src/main/java/com/example/nathanpelletier/hangman/Menu.java
+++ app/src/main/java/com/example/nathanpelletier/hangman/Menu.java
@@ -11,7 +11,7 @@ public class Menu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.Menu);
+        setContentView(R.layout.menu);
 
 //onClicks Set up ----------------------------------------
 
