18be8f2c2b3396796ef422d0851e82ec91093837
EddyNK <eddynk@github.com>
2017-11-21 00:55:22 -0700

    Made game.xml usable and ready for testing (for MVP). Usable as in the Enter button called check_answer is ready for a onclick action and the enter text field called user_guess which is a single character for now also is ready for input. I also made a chosen_word TextView to show the coder the word that was selected by the code and a guess_result TextView to show the result of the entered guess (check comparison method).

--- app/src/main/java/com/example/nathanpelletier/hangman/Game.java
+++ app/src/main/java/com/example/nathanpelletier/hangman/Game.java
@@ -1,19 +1,52 @@
 package com.example.nathanpelletier.hangman;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Game extends AppCompatActivity {
+
+    /*cannot get intent to work
+        Intent intentdata = getIntent();
+        String selectedDifficulty;
+        */
+
+
+    private String ChosenWord;
+
+
+
+    //presentation layer
+
+    private EditText user_guess;
+
+    private TextView chosen_word;
+
+    private TextView guess_result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game);
 
-    }
+        //////////////////chosen_word.setText(ChosenWord);
 
+        // TODO: 2017-11-21 Remove chosen_word view/variable
+        //only meant to reveal random word to dev
+        chosen_word = findViewById(R.id.random_word);
+
+
+        //match or nahh
+        guess_result =  findViewById(R.id.guess__result);
+
+        //user single char guess
+        user_guess =  findViewById(R.id.guessed_letter);
+
+        //Enter button
+        Button check_answer =  findViewById(R.id.enter_answer);
+
+
+    }
 }
