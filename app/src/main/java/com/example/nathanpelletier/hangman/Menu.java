0e18ccb40b9cffd54b7f23102eed6ca16d25e3cd
EddyNK <eddynk@github.com>
2017-11-21 00:30:40 -0700

    Set up onClick for all menu screen buttons (For MVP)
    Future release:
    - Sound button?

--- app/src/main/java/com/example/nathanpelletier/hangman/Menu.java
+++ app/src/main/java/com/example/nathanpelletier/hangman/Menu.java
@@ -1,7 +1,10 @@
 package com.example.nathanpelletier.hangman;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Menu extends AppCompatActivity {
 
@@ -10,6 +13,37 @@ public class Menu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.Menu);
 
+//onClicks Set up ----------------------------------------
 
+        Button EasySelect = findViewById(R.id.EasyDifficultySelect);
+        EasySelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startGame = new Intent(Menu.this, Game.class);
+                startGame.putExtra("Difficulty", R.string.Easy);
+                startActivity(startGame);
+            }
+        });
+
+        Button MediumSelect =  findViewById(R.id.MediumDifficultySelect);
+        MediumSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startGame = new Intent(Menu.this, Game.class);
+                startGame.putExtra("Difficulty", R.string.Medium);
+                startActivity(startGame);
+
+            }
+        });
+
+        Button HardSelect = findViewById(R.id.HardDifficultySelect);
+        HardSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startGame = new Intent(Menu.this, Game.class);
+                startGame.putExtra("Difficulty",  R.string.Hard);
+                startActivity(startGame);
+            }
+        });
     }
 }
