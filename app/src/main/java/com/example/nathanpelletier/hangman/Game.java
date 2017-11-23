e351b5a4a2929cdc6a1b53d44ba07ef6d214b00f
EddyNK <eddynk@github.com>
2017-11-23 16:35:57 -0700

    All methods necessary to compare user input to selected word.
    Bugs:CharCompare does not work, Need to take word from text file and not test array, difficulty is hard coded in.
    
    New Methods:
    
    ArrayLogPrint: prints input array in log(debug) with tag "lets see 0:"
    
    WordPicker: selects word depending on difficulty
    
    ChosenWord_array: creates char array filled with each character of the chosen word
    
    InputComparison: checks user input length and decides which check to use
    
    CharCompare:compares user single character guesses (when called on by InputComparison)

--- app/src/main/java/com/example/nathanpelletier/hangman/Game.java
+++ app/src/main/java/com/example/nathanpelletier/hangman/Game.java
@@ -2,6 +2,8 @@ package com.example.nathanpelletier.hangman;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -13,9 +15,15 @@ public class Game extends AppCompatActivity {
         String selectedDifficulty;
         */
 
+    String selectedDifficulty = "hard";
 
     private String ChosenWord;
 
+    private String[] test_words = {"easy", "medium", "harrrd"};
+
+    char[] checker_array;
+
+    char[] user_input_array;
 
 
     //presentation layer
@@ -26,12 +34,14 @@ public class Game extends AppCompatActivity {
 
     private TextView guess_result;
 
+    private Button check_answer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game);
 
-        //////////////////chosen_word.setText(ChosenWord);
+
 
         // TODO: 2017-11-21 Remove chosen_word view/variable
         //only meant to reveal random word to dev
@@ -45,8 +55,75 @@ public class Game extends AppCompatActivity {
         user_guess =  findViewById(R.id.guessed_letter);
 
         //Enter button
-        Button check_answer =  findViewById(R.id.enter_answer);
+        check_answer =  findViewById(R.id.enter_answer);
 
+        chosen_word.setText(ChosenWord);
 
     }
+
+    public void onStart () {
+        super.onStart();
+
+        WordPicker(selectedDifficulty);
+
+        chosen_word.setText(ChosenWord);
+
+        ChosenWord_array();
+
+        InputComparison();
+        //array that will be used for comparison
+
+    }
+
+    public void ArrayLogPrint (char[]array, String arrayname){
+        for(int x =0; x < array.length; x++) {
+            Log.d("lets see 0;" +x , ";the final array yay"+arrayname+":  " + array[x]);
+        }
+    }
+
+    public  void CharCompare(char inputletter){
+        user_input_array = new char[ChosenWord.length()];
+
+        for(int c = 0; c < checker_array.length; c++){
+            if(inputletter == checker_array[c]){
+                user_input_array[c] = inputletter;
+            }
+        }
+        ArrayLogPrint(user_input_array,"loook at me fuck with me: ");
+    }
+
+    public void InputComparison(){
+        check_answer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int UsersGuessLetterAmount  = user_guess.getText().toString().length();
+
+                if (UsersGuessLetterAmount <= 1){
+                    CharCompare(user_guess.getText().toString().charAt(0));
+                }
+            }
+        });
+    }
+
+    public void ChosenWord_array(){
+        checker_array = new char[chosen_word.length()];
+
+        for (int i = 0; i < ChosenWord.length(); i++) {
+            checker_array[i] = ChosenWord.charAt(i);
+        }
+        ArrayLogPrint(checker_array,"checkerArray");
+    }
+
+
+    //data prep
+    public void WordPicker(String selectedDifficulty) {
+        if (selectedDifficulty == "easy") {
+            ChosenWord = test_words[0];
+        } else if (selectedDifficulty == "medium") {
+            ChosenWord = test_words[1];
+        } else {
+            ChosenWord = test_words[2];
+        }
+    }
+
 }
