a67f258600dcb96604a83f7785403615646e0980
EddyNK <EddyNK@github.com>
2017-12-03 15:55:42 -0700

    fixed up spacing issue in ingame.xml

--- app/src/main/java/com/example/nathanpelletier/hangman/Keyboard.java
+++ app/src/main/java/com/example/nathanpelletier/hangman/Keyboard.java
@@ -1,7 +1,10 @@
 package com.example.nathanpelletier.hangman;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by jinzhe on 2017-11-27.
@@ -11,7 +14,29 @@ public class Keyboard extends AppCompatActivity {
 
     public void onCreate(Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
-
         setContentView(R.layout.keyboard);
     }
-}
+
+    public void onStart(){
+        super.onStart();
+
+        final Button LETTER_Q = findViewById(R.id.LetterQ);
+
+        LETTER_Q.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent KEYBOARD_INPUT = new Intent(Keyboard.this, Game.class);
+                KEYBOARD_INPUT.putExtra("key","q");
+
+                LETTER_Q.setEnabled(false);
+                LETTER_Q.setBackgroundColor(73);
+
+
+
+            }//onClick
+        });
+
+
+    }//onStart
+
+}//onCreate
