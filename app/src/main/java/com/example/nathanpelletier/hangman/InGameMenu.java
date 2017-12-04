aff8f3bce7b324308321de967284ff230de97fba
fbinsuma <fbinsuma@ualberta.ca>
2017-12-03 21:21:12 -0700

    created an inGameMenu java where it is click listener for the menu button in the game

---
+++ app/src/main/java/com/example/nathanpelletier/hangman/InGameMenu.java
@@ -0,0 +1,41 @@
+package com.example.nathanpelletier.hangman;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+
+/**
+ * Created by fatim on 2017-12-03.
+ */
+
+public class InGameMenu extends AppCompatActivity{
+
+    private static final String TAG = "InGameMenu";
+    private ImageButton menuButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ingame);
+
+        Log.d(TAG, "onCreate: Starting");
+
+        menuButton = findViewById(R.id.backToeMenuButton);
+
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: Clicked InGameMenu");
+                backToMenu();
+
+            }
+        });
+    } // onCreate(Bundle)
+
+    public void backToMenu(){
+        Intent intentMenuButton = new Intent(InGameMenu.this, Menu.class);
+        startActivity(intentMenuButton);
+    }
+}
