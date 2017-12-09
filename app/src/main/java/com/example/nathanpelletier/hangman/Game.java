8f8fa44c8e499a07b485cef8608474a34a26d0c8
jintze <Jz19940824>
2017-12-09 01:25:20 -0700

    Set background, something wrong need to figure out.

--- app/src/main/java/com/example/nathanpelletier/hangman/Game.java
+++ app/src/main/java/com/example/nathanpelletier/hangman/Game.java
@@ -232,5 +232,15 @@ public class Game extends AppCompatActivity {
     charCompare(letter);
   }
 
+  public void getDrawable(){
+      setBackground(R.layout.posterlayout);
+  }
+
+  public void posterSelector(String SELECTED_DIFFICULTY){
+    View poster = findViewById(R.id.posterLayout);
+
+    poster.setBackground(poster.getDrawable());
+  }
+
 
 } // Game
