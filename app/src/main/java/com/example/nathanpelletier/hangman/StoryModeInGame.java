6cc6775d057cf1c8332c502ff6cd2ae627856603
fbinsuma <fbinsuma@ualberta.ca>
2017-12-18 23:58:44 -0700

    Created a Story class

---
+++ app/src/main/java/com/example/nathanpelletier/hangman/StoryModeInGame.java
@@ -0,0 +1,15 @@
+package com.example.nathanpelletier.hangman;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by fatim on 2017-12-18.
+ */
+
+public class StoryModeInGame extends AppCompatActivity{
+
+    public void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+    }//onCreate
+}
